3dbb58a194ec56f5c5d8818843f92087d86b9bcc
曾宣梦 <1922254815@qq.com>
2022-01-03 19:11:30 +0800

    '实现登录'

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,16 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message
+} from 'element-ui'
+// 导入弹框
+
+Vue.prototype.$message = Message
 
 Vue.use(Button)
+Vue.use(FormItem)
+Vue.use(Form)
+Vue.use(Input)
